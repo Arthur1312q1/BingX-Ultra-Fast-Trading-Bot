d5ff3bf4ad073f6155dd046eb904234bfc76a193
Arthur1312q1 <arthur1312pc@gmail.com>
2025-12-11 23:03:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -391,12 +391,10 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get('PORT', 5000))
     
-    # Uvicorn com uvloop (mais rápido)
     uvicorn.run(
         app,
         host="0.0.0.0",
         port=port,
-        loop="uvloop",
         log_level="error",
         access_log=False
     )
